b68b8d3c32aca3b2b65f5caa67469f0e7c8d949e
Ordep-42 <p.galvao@outlook.com.br>
2023-10-17 16:26:50 -0300

    Código da Prática 3(Falta o tratamento dos dados recebidos pelo Slave)

--- PraticaSerial/master/master.ino
+++ PraticaSerial/master/master.ino
@@ -1,36 +1,58 @@
 #include <SoftwareSerial.h>
+#define ButtonPin 12
 
 SoftwareSerial ArduinoSlave(10, 11);
 
-char cmd = "";
-char old_cmd;
-char answer = "";
-char old_answer;
+String answer;
+String msg;
+String sensor;
+String botao;
+int sensorValue = 0, oldSensorValue = 0;
+int buttonValue = 0, oldButtonValue = 0;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("ENTER Commands:");
   ArduinoSlave.begin(9600);
+  pinMode(ButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  old_cmd = cmd;
-  old_answer = answer;
+  sensorValue = analogRead(A0);
+  buttonValue = !digitalRead(ButtonPin);
 
-  if (Serial.available()) {
-    cmd = Serial.read();
-  }
-  if (ArduinoSlave.available()) {
-    answer = ArduinoSlave.read();
-  }
+  sensor = String(sensorValue);
+  botao = String(buttonValue);
 
-  if (cmd != old_cmd) {
+  msg = sensor + "*" + botao;
+
+  readSlavePort();
+
+  if (oldSensorValue != sensorValue || oldButtonValue != buttonValue) {
     Serial.print("Master sent: ");
-    Serial.println(cmd);
-    ArduinoSlave.write(cmd);
+    Serial.println(msg);
+    ArduinoSlave.print(msg);
+    if (sensorValue < 20) {
+      Serial.println("Baixa luminosidade!");
+    }
+    oldSensorValue = sensorValue;
+    oldButtonValue = buttonValue;
   }
-  if (answer != old_answer) {
-    Serial.print("Slave received: ");
+
+  if (answer != "") {
+    Serial.print("Slave LED PWM Value: ");
     Serial.println(answer);
+    answer = "";
+  }
+
+  delay(500);
+}
+
+void readSlavePort() {
+  while(ArduinoSlave.available()) {
+    delay(10);
+    if (ArduinoSlave.available() > 0) {
+      char c = ArduinoSlave.read();
+      answer += c;
+    }
   }
 }
--- PraticaSerial/slave/slave.ino
+++ PraticaSerial/slave/slave.ino
@@ -1,22 +1,62 @@
 #include <SoftwareSerial.h>
 
+#define LEDPin 12;
+
 SoftwareSerial ArduinoMaster(10, 11);
 
-char cmd = "";
-char old_cmd;
+String msg = ""; 
+
+int ledVal = 0;
+int sensorVal = 0, oldSensorVal = 0;
 
 void setup() {
+  Serial.begin(9600);
   ArduinoMaster.begin(9600);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
-  old_cmd = cmd;
+  readMasterPort();
+  convertMsgToCmd();
 
-  if (ArduinoMaster.available()) {
-    cmd = ArduinoMaster.read();
+  if (sensorVal != oldSensorVal) {
+    Serial.print("Master sent: ");
+    Serial.println(sensorVal);
+
+    ledVal = map(sensorVal, 511, 0, 0, 255);
+    if (ledVal < 0){
+      ledVal = 0;
+    }
+
+    Serial.print("LED Value: ");
+    Serial.println(ledVal);
+    ArduinoMaster.print(ledVal);
+    analogWrite(12
+  , ledVal);
+
+    oldSensorVal = sensorVal;
   }
+}
 
-  if (cmd != old_cmd) {
-    ArduinoMaster.write(cmd);
+void readMasterPort() {
+  while (ArduinoMaster.available()) {
+    delay(10);
+    if (ArduinoMaster.available() > 0) {
+      char c = ArduinoMaster.read();
+      msg += c;
+    }
+  }
+  ArduinoMaster.flush();
+}
+
+void convertMsgToCmd() {
+  if (msg.length() > 0) {
+    Serial.print("message length: ");
+    Serial.println(msg.length());
+
+    char carray[6];
+    msg.toCharArray(carray, sizeof(carray));
+    sensorVal = atoi(carray);
+    msg = "";
   }
 }
