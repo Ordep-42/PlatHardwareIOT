2f0a751560380a86699c26765cf0d716db6d6ff2
Raoni Silva <raoni.silvestre@hotmail.com>
2023-12-05 17:48:03 -0300

    Add TaskScheduler library and define watering delay

--- ProjetoFinal/SistemaDeIrrigacao/include/config.h
+++ ProjetoFinal/SistemaDeIrrigacao/include/config.h
@@ -1,10 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <TaskScheduler.h>
 #include "mqttHandler.h"
 #include "sensorHandler.h"
-#include <TaskScheduler.h>
+
 #define dataRate 20000
+#define WATERINGDELAY 300000
 #define WateringRate 21600000
 
 #endif
